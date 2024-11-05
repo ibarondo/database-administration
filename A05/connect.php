0c9627b1dd5c754d8a59701489afa3e491c45950
Jannah Ibarondo <jannah.ibarondo13@gmail.com>
2024-11-06 06:50:34 +0800

    Update connect.php

--- A05/connect.php
+++ A05/connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "activity";
+	$db = "activity4";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
